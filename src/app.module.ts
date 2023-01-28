4358c3db3f371fc153072f483886f87826dae3a8
burni4 <artemnarchyk@gmail.com>
2023-01-29 01:33:03 +0300

    deploy on vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,17 @@ import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UsersRepository } from './users/users.repository';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User, UserSchema } from './users/users.schema';
 
 const mongoURILocalhost = 'mongodb://0.0.0.0:27017';
 const dbName = 'nest-homeworks-blogs';
 const mongoUri = process.env.mongoURIAtlas || mongoURILocalhost;
-
+console.log(process.env.mongoURIAtlas);
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(mongoUri),
+    MongooseModule.forRoot(process.env.mongoURIAtlas || mongoURILocalhost),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [AppController, UsersController],
